48eb71b8985e9c39f0fe2c308cc93db54364dd6a
mochimanju <parichat.ho@kkumail.com>
2025-08-20 23:42:28 +0700

    fix: deletechart and edit input style

--- src/app/main/trading/page.tsx
+++ src/app/main/trading/page.tsx
@@ -4,7 +4,7 @@ import AdvancedChart from "@/features/trading/components/Chart"
 export default function MarketOrderPage() {
   return (
     <div className="flex mx-[120px] gap-10 mt-10">
-      <div className="flex-1 "><AdvancedChart /></div>
+      <div className="flex-1 "></div>
       <div className="bg-[#081125] rounded-lg shadow-md p-5 w-[384px] h-[504px]">
         <MarketOrderContainer />
       </div>
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -15,7 +15,6 @@ function Input({ className, type, suffixIcon, ...props }: InputProps) {
           data-slot="input"
           className={cn(
             "file:text-foreground border border-transparent placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 flex h-9 w-full min-w-0 rounded-md bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow,border-color] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-            "focus:border-[#3A8AF7] focus:ring-2 focus:ring-[#3A8AF7]/20",
             "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
             className
           )}
@@ -35,7 +34,6 @@ function Input({ className, type, suffixIcon, ...props }: InputProps) {
       data-slot="input"
       className={cn(
         "file:text-foreground border border-transparent placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 flex h-9 w-full min-w-0 rounded-md bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow,border-color] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "focus:border-[#3A8AF7] focus:ring-2 focus:ring-[#3A8AF7]/20",
         "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
         className
       )}
