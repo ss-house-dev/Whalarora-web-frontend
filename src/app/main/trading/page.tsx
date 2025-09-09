cf503fd0b646b8be9c4a8769c40aaff637823235
mochimanju <parichat.ho@kkumail.com>
2025-09-09 20:34:22 +0700

    fix: layout and set fixed width/height for chart and order container

--- src/app/main/trading/page.tsx
+++ src/app/main/trading/page.tsx
@@ -4,18 +4,18 @@ import { CombinedCombobox } from "@/components/ui/combobox";
 
 export default function MarketOrderPage() {
   return (
-    <div className="mx-[36px] mt-[24px] space-y-[24px] min-h-screen">
+    <div className="mx-[23px] mt-[20px] space-y-[20px]">
       {/* Combined Combobox with Price Info */}
-      <div className="flex gap-10">
-        <div className="flex-1">
-          <CombinedCombobox />
-        </div>
+      <div className="flex-1">
+        <CombinedCombobox />
       </div>
 
       {/* Chart and Order Container side by side */}
       <div className="flex gap-[17px]">
-        <AdvancedChart />
-        <div className="bg-[#081125] rounded-lg shadow-md p-5 w-[384px] h-[507px]">
+        <div className="w-[900px] min-h-[508px]">
+          <AdvancedChart />
+        </div>
+        <div className="bg-[#081125] rounded-lg shadow-md p-5 w-[384px] h-[508px]">
           <MarketOrderContainer />
         </div>
       </div>
