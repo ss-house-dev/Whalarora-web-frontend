be15be458a770642ea874b3aaae083718181e4d2
mochimanju <parichat.ho@kkumail.com>
2025-09-09 20:02:14 +0700

    Fix active tab sync and extend active tab background to sidebar edge

---
+++ src/app/main/my-assets/page.tsx
@@ -0,0 +1,11 @@
+import MarketOrderContainer from "@/features/trading/containers/OrderContainer";
+import AdvancedChart from "@/features/trading/components/Chart";
+import { CombinedCombobox } from "@/components/ui/combobox";
+
+export default function MarketOrderPage() {
+  return (
+    <div className="mx-[36px] mt-[24px] space-y-[24px] min-h-screen">
+    <h1>My assets</h1>
+    </div>
+  );
+}
---
+++ src/app/main/my-wallet/layout.tsx
@@ -0,0 +1,40 @@
+import { Alexandria } from "next/font/google";
+import type { Metadata } from "next";
+import NavbarContainer from "@/features/wallet/containers/NavbarContainer";
+import { CoinProvider } from "@/features/trading/contexts/CoinContext";
+import Sidebar from "@/components/Sidebar";  // Import Sidebar component
+
+const alexandria = Alexandria({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Whalalora | Trading",
+  description: "Trading page",
+};
+
+export default function TradingLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className={`min-h-screen overflow-x-hidden ${alexandria.className}`}>
+      <CoinProvider>
+        {/* Fixed Navbar */}
+        <div className="fixed top-0 left-0 right-0 z-50">
+          <NavbarContainer />
+        </div>
+
+        <div className="pt-14 flex">
+          <Sidebar /> 
+
+          {/* Main Content */}
+          <div className="ml-[84px] flex-1 overflow-x-hidden">
+            {children}
+          </div>
+        </div>
+      </CoinProvider>
+    </div>
+  );
+}
