c6dd64a6757ce2a1c2ac7c1e5347a83b027287b3
nuntakarndev <nuntakarn.pa@sshouse.dev>
2025-10-02 14:31:54 +0700

    Finalize: New Holding-assets design

--- src/features/assets/components/HoldingAssetsTable.tsx
+++ src/features/assets/components/HoldingAssetsTable.tsx
@@ -54,8 +54,8 @@ export default function HoldingAssetsTable({
   }, [page, totalPages]);
 
   const sectionClasses = clsx(
-    'mt-4 w-full max-w-[1288px] rounded-2xl bg-[#16171D] px-3 py-4 sm:mt-6 sm:px-5 sm:py-5 flex flex-col',
-    isDesktopLayout && 'ml-5 my-5 h-[630px] px-6 py-6',
+    'mt-4 w-full max-w-[1304px] rounded-2xl bg-[#16171D] px-3 py-4 sm:mt-6 sm:px-5 sm:py-5 flex flex-col',
+    isDesktopLayout && 'mx-auto h-[460px] w-[1304px] px-5 py-3',
     className
   );
 
