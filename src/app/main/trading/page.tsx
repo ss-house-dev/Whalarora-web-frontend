0938db28acca8eff63395133d481f36b1f003d00
nuntakarndev <nuntakarn.pa@sshouse.dev>
2025-10-03 03:47:45 +0700

    Refactor: adjust order of components in MarketOrderPage for improved layout consistency

--- src/app/main/trading/page.tsx
+++ src/app/main/trading/page.tsx
@@ -32,23 +32,23 @@ export default function MarketOrderPage() {
   return (
     <div className="mt-[20px] space-y-[20px] px-4 pb-8 lg:px-[23px]">
       <div className="flex flex-col gap-4 lg:grid lg:grid-cols-[minmax(0,1.6fr)_360px] lg:items-start lg:gap-x-[20px] lg:gap-y-4">
-        <div className="lg:col-start-1 lg:row-start-1 lg:min-w-0">
+        <div className="order-1 lg:order-none lg:col-start-1 lg:row-start-1 lg:min-w-0">
           <CombinedCombobox className="w-full min-w-full" />
         </div>
 
-        <div className="order-1 lg:order-none lg:col-start-1 lg:row-start-2 lg:min-w-0">
+        <div className="order-2 lg:order-none lg:col-start-1 lg:row-start-2 lg:min-w-0">
           <AdvancedChart />
         </div>
 
-        <div className="order-2 w-full rounded-lg bg-[#16171D] p-4 shadow-md sm:p-5 lg:order-none lg:col-start-2 lg:row-start-2 lg:h-[508px]">
+        <div className="order-4 w-full rounded-lg bg-[#16171D] p-4 shadow-md sm:p-5 lg:order-none lg:col-start-2 lg:row-start-2 lg:h-[508px]">
           <MarketOrderContainer key={`${selectedCoin.value}-${ordersVersion}-orders`} />
         </div>
 
-        <div className="order-3 flex w-full justify-center lg:order-none lg:col-start-2 lg:row-start-3">
+        <div className="order-5 flex w-full justify-center lg:order-none lg:col-start-2 lg:row-start-3">
           <MyAssetsWidgetContainer />
         </div>
 
-        <div className="order-4 w-full lg:order-none lg:col-start-1 lg:row-start-3 lg:min-w-0">
+        <div className="order-6 w-full lg:order-none lg:col-start-1 lg:row-start-3 lg:min-w-0">
           <div className="mb-10">
             <OrderTableContainer
               activeTab={activeTab}
@@ -58,7 +58,7 @@ export default function MarketOrderPage() {
           </div>
         </div>
 
-        <div className="order-5 lg:order-none lg:col-start-2 lg:row-start-1">
+        <div className="order-3 lg:order-none lg:col-start-2 lg:row-start-1">
           <OrderBookLiveContainer className="h-full w-full" showMetaInfo={false} />
         </div>
       </div>
